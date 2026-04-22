571c77a313daa7b07e1a1e4e52d2d6cfb19cd214
DODAM <djlim125@gmail.com>
2026-04-22 20:10:16 +0900

    feat: add drawer widget

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class ProfileDrawer extends SataelessWidget{
-    @override
-    Widget build(BuildContext context) {
-     return Container(
-        width: 200,
-        height: double.infinity,
-        color: Colors.blue,
-     );
-   }
+class ProfileDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: double.infinity,
+      color: Colors.blue,
+    );
+  }
 }
