553a16dd9f482d27a4c5e6db8c292a1d57582946
DODAM <djlim125@gmail.com>
2026-04-23 15:43:25 +0900

    feat: add header widget

---
+++ lib/components/header.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ProfileHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const SizedBox(width: 20),
+        _buildHeaderAvatar(),
+        const SizedBox(width: 20),
+        _buildHeaderProfile(),
+      ],
+    );
+  }
+
+  Widget _buildHeaderAvatar() {
+    return const SizedBox(
+      width: 100,
+      height: 100,
+      child: CircleAvatar(
+        backgroundImage: AssetImage("assets/dodam.jpg"),
+      ),
+    );
+  }
+
+  Widget _buildHeaderProfile() {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "LIM DODAM",
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        Text(
+          "maltipoo",
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+      ],
+    );
+  }
+}
